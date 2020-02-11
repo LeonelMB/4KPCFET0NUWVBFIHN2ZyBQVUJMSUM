e41991b14bc10955d5922cb95035fa528b63d63e
l0m01bp <Leonel.Morales@walmart.com>
2020-02-10 18:24:29 -0600

    chain approx

--- script.js
+++ script.js
@@ -66,60 +66,97 @@ function startVideoProcessing() {
     requestAnimationFrame(processVideo);
 }
 
+
+// function passThrough(src) {
+//     let color = new cv.Scalar(0, 0, 255);
+//     let circles = new cv.Mat(height, width, cv.CV_8UC4);
+//     const dst = new cv.Mat();
+//     // change to hsv colorspace
+//     const hsv = new cv.Mat();
+//     try {
+
+//         cv.cvtColor(src, hsv, cv.COLOR_BGR2HSV)
+//             // HSV yellow range
+//         const lower_white = [5, 100, 100, 255];
+//         const upper_white = [100, 255, 255, 255];
+//         // HSV range filters
+//         const low = new cv.Mat(hsv.rows, hsv.cols, hsv.type(), lower_white);
+//         const high = new cv.Mat(hsv.rows, hsv.cols, hsv.type(), upper_white);
+//         // filter by range
+//         cv.inRange(hsv, low, high, dst);
+//         // Searching circles
+//         cv.medianBlur(dst, dstC4, 3);
+//         cv.HoughCircles(dstC4, circles, cv.HOUGH_GRADIENT, 1, hsv.rows / 128, 100, 10, 5, 15);
+
+//         if (circles.size().width > 5) {
+//             let xPointMean = 0;
+//             let yPointMean = 0;
+//             let radiusMean = 0;
+//             let NofPoints = circles.cols;
+//             // draw circles 
+//             for (let i = 0; i < circles.cols; ++i) {
+//                 let x = circles.data32F[i * 3];
+//                 let y = circles.data32F[i * 3 + 1];
+//                 let radius = circles.data32F[i * 3 + 2];
+//                 let center = new cv.Point(x, y);
+//                 cv.circle(src, center, radius, color, thickness = 1);
+//                 xPointMean += x;
+//                 yPointMean += y;
+//                 radiusMean += radius;
+//             }
+//             xPointMean = xPointMean / NofPoints;
+//             yPointMean = yPointMean / NofPoints;
+//             //  get the mean point of the circles
+//            // cv.circle(src, new cv.Point(xPointMean, yPointMean), radiusMean, color, thickness = 1);
+//             renderEarth(xPointMean / width, yPointMean / height, radiusMean / width);
+//         } else {
+//             circles.delete();
+//         }
+//         dst.delete();
+//         low.delete();
+//         high.delete();
+//         //hsv.delete();
+//     } catch (e) {
+//         console.warn(e)
+//     }
+//     return src;
+// }
 function passThrough(src) {
-    let color = new cv.Scalar(0, 0, 255);
-    let circles = new cv.Mat(height, width, cv.CV_8UC4);
-    const dst = new cv.Mat();
 
-    try {
-        // change to hsv colorspace
-        const hsv = new cv.Mat();
-        cv.cvtColor(src, hsv, cv.COLOR_BGR2HSV)
-            // HSV yellow range
-        const lower_white = [10, 100, 100, 255];
-        const upper_white = [100, 255, 255, 255];
-        // HSV range filters
-        const low = new cv.Mat(hsv.rows, hsv.cols, hsv.type(), lower_white);
-        const high = new cv.Mat(hsv.rows, hsv.cols, hsv.type(), upper_white);
-        // filter by range
-        cv.inRange(hsv, low, high, dst);
-        // Searching circles
-        cv.medianBlur(dst, dstC4, 3);
-        cv.HoughCircles(dstC4, circles, cv.HOUGH_GRADIENT, 1, hsv.rows / 64, 50, 20);
 
-        if (circles.size().width > 5) {
-            let xPointMean = 0;
-            let yPointMean = 0;
-            let radiusMean = 0;
-            let NofPoints = circles.cols;
-            // draw circles 
-            for (let i = 0; i < circles.cols; ++i) {
-                let x = circles.data32F[i * 3];
-                let y = circles.data32F[i * 3 + 1];
-                let radius = circles.data32F[i * 3 + 2];
-                let center = new cv.Point(x, y);
-                cv.circle(src, center, radius, color, thickness = 1);
-                xPointMean += x;
-                yPointMean += y;
-                radiusMean += radius;
-            }
-            xPointMean = xPointMean / NofPoints;
-            yPointMean = yPointMean / NofPoints;
-            /* radiusMean = radiusMean / N;*/
-            //  get the mean point of the circles
-            cv.circle(src, new cv.Point(xPointMean, yPointMean), radiusMean, color, thickness = 1);
-            renderEarth(xPointMean / width, yPointMean / height, radiusMean / width);
-        } else {
-            circles.delete();
+    // let mat = new cv.Mat(height, width, cv.CV_8U);
+    cv.cvtColor(src, dstC1, cv.COLOR_RGBA2GRAY);
+    // cv.adaptiveThreshold(mat, dstC1, 200, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 30);
+    // mat.delete();
+
+    cv.threshold(dstC1, dstC4, 120, 200, cv.THRESH_BINARY);
+    let contours = new cv.MatVector();
+    let hierarchy = new cv.Mat();
+    cv.findContours(dstC4, contours, hierarchy, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE, { x: 0, y: 0 });
+    dstC3.delete();
+    dstC3 = cv.Mat.ones(height, width, cv.CV_8UC3);
+    for (let i = 0; i < contours.size(); ++i) {
+        let k = i;
+        let c = 0;
+        while (hierarchy.intPtr(0, k)[2] !== -1) {
+            k = hierarchy.intPtr(0, k)[2]
+            c = c + 1;
         }
-        dst.delete();
-        low.delete();
-        high.delete();
-        hsv.delete();
-    } catch (e) {
-        console.warn(e)
+
+        if (hierarchy.intPtr(0, k)[2] !== -1) {
+            c = c + 1;
+        }
+        if (c >= 2) {
+            console.log(c);
+            let color = contoursColor[i];
+            cv.drawContours(dstC3, contours, i, color, 1, cv.LINE_8, hierarchy);
+        }
+
     }
-    return src;
+    contours.delete();
+    hierarchy.delete();
+    // return dstC3;
+    return dstC3;
 }
 
 function renderEarth(x, y, radius) {
